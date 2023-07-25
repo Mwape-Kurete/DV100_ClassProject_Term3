5a0b20a7054e1af403654d44825cde74d4ffa859
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2023-07-26 01:37:11 +0200

    adding images and doing homework

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    console.log("working"); 
+
+    // when the hero loads animate it upwards
+
+    $(".hero-image").animate({top: '-=100px'})
+
+    $(".hero-image2").animate({top: '-=50px'})
+});
