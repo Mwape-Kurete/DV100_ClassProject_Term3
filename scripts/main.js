81918d7b75a94e044a3b0756b69613be6bbfcee6
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2023-08-01 19:25:09 +0200

    final js added for browse page

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
 
     console.log("working"); 
-
+    
+    // --------------------------------------------
+    // Home Page
     // when the hero loads animate it upwards
+    
+    $(".hero-image").animate({top: '-=100px'});
 
-    $(".hero-image").animate({top: '-=100px'})
 
-    $(".hero-image2").animate({top: '-=50px'})
+    // ----------------------------------------------
+    // browse 
+    $("#descriptionText").hide();
 });
+
+// When card is clicked 
+
+$(".card").click(function(){
+    
+    // TOGGLE PRICE AND DESCRIPT TEXT
+
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // resize image to fit added text 
+    $(".card-img-top").toggleClass("small");
+})
